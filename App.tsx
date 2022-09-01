e2bc5ac0b976955a82d20c515c77f23d8a0ceb88
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-09-01 11:39:14 +0530

    Final.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import RootStack from './src/navigation/RootStack';
 
-const App = () => <RootStack />;
+const App = () => (
+  <>
+    <StatusBar hidden />
+    <RootStack />
+  </>
+);
 
 export default App;
